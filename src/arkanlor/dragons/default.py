8ef10de1facf6b5cb495c0bcc395090193985b86
Gabor Guzmics <gab@g4b.org>
2012-09-25 14:01:03 +0200

    default filter setup in try..pass

--- src/arkanlor/dragons/default.py
+++ src/arkanlor/dragons/default.py
@@ -3,5 +3,14 @@ from django.conf import settings
 from arkanlor.dragons.quanum import QuanumQnor
 from arkanlor.dragons.dragon import DragonScripts
 
-quanum = QuanumQnor(settings.QUANUM_SCRIPT_DIR)
-dragon_scripts = DragonScripts(settings.DRAGON_SCRIPT_DIR)
+try:
+    quanum = QuanumQnor(settings.QUANUM_SCRIPT_DIR)
+except:
+    print "Exception occured. QuanumQnor not initialized."
+    quanum = None
+
+try:
+    dragon_scripts = DragonScripts(settings.DRAGON_SCRIPT_DIR)
+except:
+    print "Exception occured. DragonScripts not initialized."
+    dragon_scripts = None
